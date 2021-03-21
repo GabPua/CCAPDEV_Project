1dd6b827e663bf5e2da8fd7ea7729d54a9345575
Jared Sy <jaredblase@gmail.com>
2021-03-21 11:28:23 +0800

    docs(DatabaseTable.php): added documentation

--- classes/generic/DatabaseTable.php
+++ classes/generic/DatabaseTable.php
@@ -2,6 +2,15 @@
 namespace generic;
 use PDO, PDOStatement, PDOException;
 
+/**
+ * A framework class to access any table in an SQL database.
+ * 
+ * Given a PDO, a string representing the table name, and a string represeting the
+ * name of the primary key, this class will immediately be able to access the database.
+ * It contains the common methods/interactions with a database.
+ * 
+ * @since 1.0.0
+ */
 class DatabaseTable
 {
     public function __construct(
@@ -10,6 +19,16 @@ class DatabaseTable
         private string $primaryKey
     ) {}
 
+    /**
+     * Receives an SQL statement and executes it.
+     * 
+     * @since 1.0.0
+     * 
+     * @param string $sql The SQL statement to be excuted.
+     * @param array $parameters the parameters to be inserted in the prepared statement.
+     * 
+     * @return PDOStatement $query The result of the exectuted query.
+     */
     private function query($sql, $parameters = []): PDOStatement
     {
         $query = $this->pdo->prepare($sql);
@@ -17,12 +36,28 @@ class DatabaseTable
         return $query;
     }
 
+    /**
+     * Returns the number of records in a table.
+     * 
+     * @since 1.0.0
+     * 
+     * @return int The number of records in a table.
+     */
     public function total()
     {
         $query = $this->query("SELECT COUNT(*) FROM $this->table");
         return $query->fetch()[0];
     }
 
+    /**
+     * Return the record that matches with the given primary key value.
+     * 
+     * @since 1.0.0
+     * 
+     * @param mixed $value The primary key value.
+     * 
+     * @return PDOStatement The record that matches with the given primary key.
+     */
     public function findById($value)
     {
         $query = "SELECT * FROM $this->table WHERE $this->primaryKey = :value";
@@ -31,7 +66,14 @@ class DatabaseTable
         return $this->query($query, $parameters)->fetch();
     }
 
-
+    /**
+     * Inserts a record into the database.
+     * 
+     * @since 1.0.0
+     * 
+     * @param array $fields Contains the values of the record to be inserted into the database with the keys 
+     *                      of the arrays being the table's column names.
+     */
     private function insert($fields)
     {
         $query = 'INSERT INTO `' . $this->table . '` (';
@@ -58,7 +100,14 @@ class DatabaseTable
         $this->query($query, $fields);
     }
 
-
+    /**
+     * Updates a record into the database.
+     * 
+     * @since 1.0.0
+     * 
+     * @param array $fields Contains the values of the record to be updated in the database with the keys 
+     *                      of the arrays being the table's column names.
+     */
     private function update($fields)
     {
         $query = ' UPDATE `' . $this->table . '` SET ';
@@ -79,7 +128,13 @@ class DatabaseTable
         $this->query($query, $fields);
     }
 
-
+    /**
+     * Deletes a record in the database.
+     * 
+     * @since 1.0.0
+     * 
+     * @param mixed $id Contains the primary key value of the record to be deleted.
+     */
     public function delete($id)
     {
         $parameters = [':id' => $id];
@@ -87,7 +142,13 @@ class DatabaseTable
         $this->query('DELETE FROM `' . $this->table . '` WHERE `' . $this->primaryKey . '` = :id', $parameters);
     }
 
-
+    /**
+     * Returns all the records in the database.
+     * 
+     * @since 1.0.0
+     * 
+     * @return PDOStatement An array contaning all the records in the database.
+     */
     public function findAll()
     {
         $result = $this->query('SELECT * FROM ' . $this->table);
@@ -95,6 +156,14 @@ class DatabaseTable
         return $result->fetchAll();
     }
 
+    /**
+     * Standardizes any DateTime object in the values to match with the format
+     * required by the database.
+     * 
+     * @since 1.0.0
+     * 
+     * @return array $fields The same set of elements, but all DataTime objects have been formatted.
+     */
     private function processDates($fields)
     {
         foreach ($fields as $key => $value) {
@@ -106,7 +175,17 @@ class DatabaseTable
         return $fields;
     }
 
-
+    /**
+     * Updates or inserts a record into the database.
+     * 
+     * A public method for convenience since users will not have to think
+     * whether to use update or insert while coding.
+     * 
+     * @since 1.0.0
+     * 
+     * @param array $record Contains the values of the record to be updated in the database with the keys 
+     *                      of the arrays being the table's column names.
+     */
     public function save($record)
     {
         try {
