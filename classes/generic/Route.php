4a71c88d28331a2b9b9ba4f3064a71b3d1bbcd5f
Jared Sy <jaredblase@gmail.com>
2021-03-21 11:31:05 +0800

    docs(Route.php): added documentation

--- classes/generic/Route.php
+++ classes/generic/Route.php
@@ -1,6 +1,16 @@
 <?php
 namespace generic;
 
+/**
+ * Created to enable type hinting in EntryPoint.
+ * 
+ * A class implementing this interface should return an array that contains
+ * the controller to use and the method to use given a user's input.
+ * 
+ * @since 1.0.0
+ * 
+ * @see EntryPoint
+ */
 interface Route
 {
     public function getRoutes();
