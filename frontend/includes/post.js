f0fc66d8cb5c975acb4f4ef6a8ff15121a86a806
GabPua <gabriel_pua@dlsu.edu.ph>
2021-04-05 20:38:23 +0800

    add: pictures and recipe posts

--- frontend/includes/post.js
+++ frontend/includes/post.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
     const tab = $('.tabs li');
     const pic = $('.food-picture');
 
+    const upvote = $('#upvote');
+    const downvote = $('#downvote');
+    const votes = $('#votes');
+
     tab.on('click', function () {
         let val = $(this).children().html();
 
@@ -12,5 +16,39 @@ $(document).ready(function () {
         let newUrl = url.substr(0, url.length - 5) + val + '.jpg';
 
         pic.attr('src', newUrl);
+    });
+
+    upvote.on('click', function () {
+        if (upvote[0].classList.contains('is-active')) {
+            upvote.removeClass('is-active');
+            let num_votes = Number(votes.html());
+            votes.html(num_votes - 1);
+        } else {
+            let num_votes = Number(votes.html());
+
+            if (downvote[0].classList.contains('is-active')) {
+                downvote.removeClass('is-active');
+                num_votes += 1;
+            }
+            upvote.addClass('is-active');
+            votes.html(num_votes + 1);
+        }
+    });
+
+    downvote.on('click', function () {
+        if (downvote[0].classList.contains('is-active')) {
+            downvote.removeClass('is-active');
+            let num_votes = Number(votes.html());
+            votes.html(num_votes + 1);
+        } else {
+            let num_votes = Number(votes.html());
+
+            if (upvote[0].classList.contains('is-active')) {
+                upvote.removeClass('is-active');
+                num_votes -= 1;
+            }
+            downvote.addClass('is-active');
+            votes.html(num_votes - 1);
+        }
     });
 });
