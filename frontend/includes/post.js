a176b0a15e1754e8aac7cdf8a99396768846887f
GabPua <gabriel_pua@dlsu.edu.ph>
2021-04-06 22:12:44 +0800

    add: changing of image in post now functional

--- frontend/includes/post.js
+++ frontend/includes/post.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
     const carousel = $('.post-header .pagination a.pagination-link');
-    const pic = $('.slider');
+    const pic = $('#food-pic');
+
+    const left = $('#left');
+    const right = $('#right');
 
     const upvote = $('#upvote');
     const downvote = $('#downvote');
@@ -12,10 +15,44 @@ $(document).ready(function () {
         carousel.removeClass('is-current');
         $(this).addClass('is-current');
 
-        let url = pic.attr('background');
-        let newUrl = url.substr(0, url.length - 5) + val + '.jpg';
+        let url = pic.attr('style');
+        let newUrl = url.substr(0, url.length - 7) + val + '.jpg")';
 
-        pic.attr('background', 'url("' + newUrl + ')');
+        pic.attr('style', newUrl);
+    });
+
+    left.on('click', function () {
+        const curr = $('.post-header .pagination a.pagination-link.is-current');
+
+        let val = Number(curr.attr('aria-label').slice(-1)) - 1;
+        if (val < 1) val += 4;
+
+        const next = $('.post-header .pagination').find('[aria-label=\'image ' + val + '\']');
+
+        curr.removeClass('is-current');
+        next.addClass('is-current');
+
+        let url = pic.attr('style');
+        let newUrl = url.substr(0, url.length - 7) + val + '.jpg")';
+
+        pic.attr('style', newUrl);
+    });
+
+    right.on('click', function () {
+        const curr = $('.post-header .pagination a.pagination-link.is-current');
+
+        let val = Number(curr.attr('aria-label').slice(-1)) + 1;
+        if (val > 4) val -= 4;
+        
+        const next = $('.post-header .pagination').find('[aria-label=\'image ' + val + '\']');
+
+        curr.removeClass('is-current');
+        next.addClass('is-current');
+
+        let url = pic.attr('style');
+        let newUrl = url.substr(0, url.length - 7) + val + '.jpg")';
+
+        pic.attr('style', newUrl);
     });
 
     upvote.on('click', function () {
