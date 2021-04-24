8312bc20e85ba332c71626316e3e9710b2dd5bb8
Jared <jaredblase@gmail.com>
2021-04-24 21:22:18 +0800

    update: models

---
+++ models/comment.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+let commentSchema = new mongoose.Schema({
+    // TODO: complete comment schema
+})
+
+module.exports = mongoose.model('Comment', commentSchema);
---
+++ models/recipe.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+let recipeSchema = new mongoose.Schema({
+    user_id: {type: String, require: true},
+    title: {type: String, require: true},
+    desc: {type: String, require: false},
+    serving: {type: Number, require: true},
+    picture: {type: Array, require: true},
+    prep_time: {type: Number, require: true},
+    cook_time: {type: Number, require: true},
+    direction: {type: String, require: true},
+    ingredients: {type: String, require: true},
+    like: {type: Array, require: true},
+    dislikes: {type: Array, require: true}
+});
+
+module.exports = mongoose.model('Recipe', recipeSchema);
