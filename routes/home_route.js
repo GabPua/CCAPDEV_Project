c40e5a7642eee801ce3e92b0dfbbffff9de3b1b9
Jared <jaredblase@gmail.com>
2021-04-25 03:48:59 +0800

    update: new routes

--- routes/home_route.js
+++ routes/home_route.js
@@ -2,11 +2,26 @@ const express = require('express');
 const router = express.Router();
 const homeTitle = 'ShefHub | Free Recipes & More';
 
-router.get('/', function (req, res) {
-    // show newsfeed if logged in, else show homepage
+function verifyUser (req, res) {
+    console.log(req.session);
+    console.log(req.cookies);
+    if (!(req.session.name && req.cookies.user_sid)) {
+        res.redirect('/');
+    }
+}
+
+const sessionChecker = (req, res, next) => {
+    if (req.session.name && req.cookies.user_sid) {
+        res.redirect('/newsfeed');
+    } else {
+        next();
+    }
+}
+
+router.get('/', sessionChecker, (req, res) => {
     // req.session.name = 'MasterChef1001';
-    res.render(req.session.name? 'newsfeed' : 'home', {
-        title: homeTitle,
+    res.render('home', {
+        title: homeTitle
     });
 });
 
@@ -22,18 +37,42 @@ router.get('/explore', function(req, res) {
     });
 });
 
-router.get('/signup', function(req, res) {
-    res.render('signup', {
-        title: 'Sign up @ ShefHub',
-        signup: true
+router.route('/signup')
+    .get((req, res) => {
+        res.render('signup', {
+            title: 'Sign up @ ShefHub',
+            signup: true
+        });
+    })
+    .post((req, res) => {
+        // TODO: registration
     });
-});
 
 router.get('/login', function(req, res) {
     res.render('login', {
         title: 'Log in @ ShefHub',
         login: true
     });
+});
+
+// logged-in only pages
+
+router.get('/logout', function(req, res) {
+    verifyUser(req, res);
+    req.session.destroy();
+    res.clearCookie('user_sid');
+    res.redirect('/');
+});
+
+router.get('/:view', (req, res, next) => {
+    verifyUser(req, res);
+    try {
+        res.render(req.params.view, {
+            title: "HELLO"
+        });
+    } catch (err) {
+        next();
+    }
 });
 
 module.exports = router;
