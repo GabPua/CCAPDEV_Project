a73e1e412db59e9c0feeff3520f02a534c78bc18
Jared <jaredblase@gmail.com>
2021-04-24 22:44:12 +0800

    update(home_route): added new routes

--- routes/home_route.js
+++ routes/home_route.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const homeTitle = 'ShefHub | Free Recipes & More';
 
 router.get('/', function (req, res) {
-    res.render('home', {
+    // show newsfeed if logged in, else show homepage
+    // req.session.name = 'MasterChef1001';
+    res.render(req.session.name? 'newsfeed' : 'home', {
         title: homeTitle,
     });
 });
@@ -11,13 +13,27 @@ router.get('/', function (req, res) {
 router.get('/about', function(req, res) {
     res.render('about', {
         title: homeTitle
-    })
-})
+    });
+});
 
 router.get('/explore', function(req, res) {
     res.render('explore', {
         title: homeTitle
-    })
-})
+    });
+});
+
+router.get('/signup', function(req, res) {
+    res.render('signup', {
+        title: 'Sign up @ ShefHub',
+        signup: true
+    });
+});
+
+router.get('/login', function(req, res) {
+    res.render('login', {
+        title: 'Log in @ ShefHub',
+        login: true
+    });
+});
 
 module.exports = router;
