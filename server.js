e024265e51d26c32d257ba453f5ae5ec2e9a3ee9
Jared <jaredblase@gmail.com>
2021-04-24 18:32:47 +0800

    refactor: placed index.js contents inside server.js

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const hbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+// get environment variables
+dotenv.config();
+const port = process.env.PORT || 3000;
+const hostname = process.env.HOSTNAME || 'localhost';
 const dbUri = process.env.SERVER_DB_URI;
 
-// setup connection with mongodb
+// setup connection with mongo db
 mongoose.connect(dbUri, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(
         () => {
@@ -13,4 +23,28 @@ mongoose.connect(dbUri, {useNewUrlParser: true, useUnifiedTopology: true})
         }
     );
 
-module.exports = mongoose.connection;
+const db = mongoose.connection;
+
+// initialize express
+const app = express();
+app.use(cookieParser());
+app.use(session({secret: 'hushPuppy', reSave: true}));
+app.engine('hbs', hbs({extname: '.hbs'}));
+app.set('view engine', 'hbs');
+
+// routes
+const home_route = require('./routes/home_route');
+
+app.use('/public', express.static('public'));
+app.use('/', home_route);
+
+// Error 404: File not found
+app.use(function (req, res) {
+    res.status(404).send('File not in server!');
+});
+
+// initialize server
+app.listen(port, hostname,() => {
+    console.log('Server running at: ');
+    console.log('http://' + hostname + ':' + port + '\n');
+});
