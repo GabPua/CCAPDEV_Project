1afe6b2bbb9ff158ca667238c5420339b0af5058
Jared <jaredblase@gmail.com>
2021-04-26 00:38:04 +0800

    fix: moved sessionChecker() to home_ctrl

--- routes/home_route.js
+++ routes/home_route.js
@@ -2,16 +2,6 @@ const express = require('express');
 const ctrl = require('../controllers/home_ctrl');
 const router = express.Router();
 
-
-
-const sessionChecker = (req, res, next) => {
-    if (req.session.name && req.cookies.user_sid) {
-        res.redirect('/newsfeed');
-    } else {
-        next();
-    }
-}
-
 router.get('/', ctrl.getIndex);
 
 router.get('/about', ctrl.getAbout);
