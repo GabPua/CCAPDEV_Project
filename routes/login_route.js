d49207562ae8aae53c83e41fdb327fc34632fe4b
Jared <jaredblase@gmail.com>
2021-04-29 01:42:20 +0800

    code cleanup: routes

--- routes/login_route.js
+++ routes/login_route.js
@@ -1,9 +1,8 @@
-const express = require('express');
+const router = require('express')();
 const ctrl = require('../controllers/login_ctrl');
-const router = express.Router();
 
-router.get('/', ctrl.getLogin);
-
-router.post('/', ctrl.postLogin);
+router.route('/')
+    .get(ctrl.getLogin)
+    .post(ctrl.postLogin);
 
 module.exports = router;
