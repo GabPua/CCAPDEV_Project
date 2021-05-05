bcd9ffe12e21155090dbf6ad208107d1f1867e12
Jared <jaredblase@gmail.com>
2021-05-05 22:24:07 +0800

    add: comments in post now load

--- models/comment.js
+++ models/comment.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 let commentSchema = new mongoose.Schema({
-    // TODO: complete comment schema
+    user: {type: String, require: true, ref: 'User'},
+    recipe: {type: mongoose.ObjectId, require: true, ref: 'Recipe'},
+    reply_to: {type: mongoose.ObjectId, require: false, ref: 'Comment'},
+    body: {type: String, require: true},
 });
 
 module.exports = mongoose.model('Comment', commentSchema, 'comment');
