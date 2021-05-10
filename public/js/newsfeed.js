6abc7bbb891fde7f228b6cfdc69a86d320892778
GabPua <gabriel_pua@dlsu.edu.ph>
2021-05-10 23:19:57 +0800

    add: voting on posts now works

--- public/js/newsfeed.js
+++ public/js/newsfeed.js
@@ -1,41 +1,81 @@
 $(document).ready(function() {
     $('.upvote').on('click', function () {
         let count = $(this).siblings('.display-count');
+        let num_votes = Number(count.html());
+        let downvote = $(this).siblings('.downvote');
+        let recipe_id = $(this).siblings('input').val();
 
         if ($(this).hasClass('is-active')) {
             $(this).removeClass('is-active');
-            let num_votes = Number(count.html());
             count.html(num_votes - 1);
-        } else {
-            let num_votes = Number(count.html());
-            let downvote = $(this).siblings('.downvote');
 
-            if (downvote.hasClass('is-active')) {
-                downvote.removeClass('is-active');
-                num_votes += 1;
-            }
+            $.post('/vote/delete', {recipe_id: recipe_id}, (success) => {
+                if (success)
+                    console.log('Vote deleted successfully');
+                else
+                    console.log('Error deleting vote');
+            });
+        } else if (downvote.hasClass('is-active')) {
+            downvote.removeClass('is-active');
+            $(this).addClass('is-active');
+            count.html(num_votes + 2);
+
+            $.post('/vote/update', {recipe_id: recipe_id, vote_value: 1}, (success) => {
+                if (success)
+                    console.log('Vote updated successfully');
+                else
+                    console.log('Error updating vote');
+            });
+        } else {
             $(this).addClass('is-active');
             count.html(num_votes + 1);
+
+            $.post('/vote/add', {recipe_id: recipe_id, vote_value: 1}, (success) => {
+                if (success)
+                    console.log('Voted successfully');
+                else
+                    console.log('Error voting');
+            });
         }
     });
 
     $('.downvote').on('click', function () {
         let count = $(this).siblings('.display-count');
+        let num_votes = Number(count.html());
+        let upvote = $(this).siblings('.upvote');
+        let recipe_id = $(this).siblings('input').val();
 
         if ($(this).hasClass('is-active')) {
             $(this).removeClass('is-active');
-            let num_votes = Number(count.html());
             count.html(num_votes + 1);
-        } else {
-            let num_votes = Number(count.html());
-            let upvote = $(this).siblings('.upvote');
 
-            if (upvote.hasClass('is-active')) {
-                upvote.removeClass('is-active');
-                num_votes -= 1;
-            }
+            $.post('/vote/delete', {recipe_id: recipe_id}, (success) => {
+                if (success)
+                    console.log('Vote deleted successfully');
+                else
+                    console.log('Error deleting vote');
+            });
+        } else if (upvote.hasClass('is-active')) {
+            upvote.removeClass('is-active');
+            $(this).addClass('is-active');
+            count.html(num_votes - 2);
+
+            $.post('/vote/update', {recipe_id: recipe_id, vote_value: 1}, (success) => {
+                if (success)
+                    console.log('Vote updated successfully');
+                else
+                    console.log('Error updating vote');
+            });
+        } else {
             $(this).addClass('is-active');
             count.html(num_votes - 1);
+
+            $.post('/vote/add', {recipe_id: recipe_id, vote_value: 1}, (success) => {
+                if (success)
+                    console.log('Voted successfully');
+                else
+                    console.log('Error voting');
+            });
         }
     });
 });
--- public/js/post.js
+++ public/js/post.js
@@ -175,36 +175,76 @@ $(document).ready(function () {
         });
 
         upvote.on('click', function () {
+            let num_votes = Number(votes.html());
+
             if (upvote.hasClass('is-active')) {
                 upvote.removeClass('is-active');
-                let num_votes = Number(votes.html());
                 votes.html(num_votes - 1);
-            } else {
-                let num_votes = Number(votes.html());
 
-                if (downvote.hasClass('is-active')) {
-                    downvote.removeClass('is-active');
-                    num_votes += 1;
-                }
+                $.post('/vote/delete', {recipe_id: recipe_id}, (success) => {
+                    if (success)
+                        console.log('Vote deleted successfully');
+                    else
+                        console.log('Error deleting vote');
+                });
+            } else if (downvote.hasClass('is-active')) {
+                downvote.removeClass('is-active');
+                upvote.addClass('is-active');
+                votes.html(num_votes + 2);
+
+                $.post('/vote/update', {recipe_id: recipe_id, vote_value: 1}, (success) => {
+                    if (success)
+                        console.log('Vote updated successfully');
+                    else
+                        console.log('Error updating vote');
+                });
+            } else {
                 upvote.addClass('is-active');
                 votes.html(num_votes + 1);
+
+                $.post('/vote/add', {recipe_id: recipe_id, vote_value: 1}, (success) => {
+                    if (success)
+                        console.log('Voted successfully');
+                    else
+                        console.log('Error voting');
+                });
             }
         });
 
         downvote.on('click', function () {
+            let num_votes = Number(votes.html());
+
             if (downvote.hasClass('is-active')) {
                 downvote.removeClass('is-active');
-                let num_votes = Number(votes.html());
                 votes.html(num_votes + 1);
-            } else {
-                let num_votes = Number(votes.html());
 
-                if (upvote.hasClass('is-active')) {
-                    upvote.removeClass('is-active');
-                    num_votes -= 1;
-                }
+                $.post('/vote/delete', {recipe_id: recipe_id}, (success) => {
+                    if (success)
+                        console.log('Vote deleted successfully');
+                    else
+                        console.log('Error deleting vote');
+                });
+            } else if (upvote.hasClass('is-active')) {
+                upvote.removeClass('is-active');
+                downvote.addClass('is-active');
+                votes.html(num_votes - 2);
+
+                $.post('/vote/update', {recipe_id: recipe_id, vote_value: -1}, (success) => {
+                    if (success)
+                        console.log('Vote updated successfully');
+                    else
+                        console.log('Error updating vote');
+                });
+            } else {
                 downvote.addClass('is-active');
                 votes.html(num_votes - 1);
+
+                $.post('/vote/add', {recipe_id: recipe_id, vote_value: -1}, (success) => {
+                    if (success)
+                        console.log('Voted successfully');
+                    else
+                        console.log('Error voting');
+                });
             }
         });
 
