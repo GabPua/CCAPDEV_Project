0babc6add5d66a8c9c8a39fe9dc4443c33c2f4c7
Jared <jaredblase@gmail.com>
2021-05-11 13:13:13 +0800

    update: delete user completed

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Follow = require('./follow');
 const Vote = require('./vote');
 const Post = require('./recipe');
+const Comment = require('./comment');
 const async = require('async')
 
 let userSchema = new mongoose.Schema({
@@ -17,14 +18,8 @@ let userSchema = new mongoose.Schema({
 userSchema.post(['deleteOne', 'findOneAndDelete'], (user) => {
 
     async.series([
-        function deleteFollowing(callback) {
-            Follow.deleteMany({follower: user._id}, (err, result) => {
-                callback(err, result);
-            });
-        },
-
-        function deleteFollowers(callback) {
-            Follow.deleteMany({following: user._id}, (err, result) => {
+        function deleteFollows(callback) {
+            Follow.deleteMany({ $or: [{follower: user._id}, {following: user._id}] }, (err, result) => {
                 callback(err, result);
             });
         },
@@ -36,15 +31,63 @@ userSchema.post(['deleteOne', 'findOneAndDelete'], (user) => {
         },
 
         function deletePosts(callback) {
-        // TODO: delete likes and comments related to posts
-            Post.deleteMany({user: user._id}, (err, result) => {
+            async.waterfall([
+                function findPosts(callback) {
+                    Post.find({user: user._id}, '_id', (err, result) => {
+                        callback(err, result)
+                    }).lean();
+                },
+
+                function deletePosts(posts, callback) {
+                    console.log(posts);
+                    async.each(posts, function deleteEachPosts (post, callback) {
+                        // this also delete likes and comments related to the post, defined in recipe model
+                        Post.findOneAndDelete({_id: post._id}, (err) => {
+                            callback(err);
+                        }).lean();
+                    }, (err) => {
+                        callback(err, 'SUCCESS');
+                    });
+                },
+            ], (err, result) => {
                 callback(err, result);
             });
         },
 
-        // TODO: Delete user's comments and comments of others on user's posts
+        function deleteComments(callback) {
+            async.waterfall([
+                function findComments(callback) {
+                    Comment.find({user: user._id}, '_id', (err, result) => {
+                        callback(err,result);
+                    }).lean();
+                },
 
-    ], );
+                function deleteComments(comments, callback) {
+                    console.log(comments);
+
+                    async.each(comments, function deleteEachComment (comment, callback)  {
+                        // this also deletes replies to the comment, defined in comment model
+                            Comment.findOneAndDelete({_id: comment._id}, (err) => {
+                               callback(err);
+                            }).lean();
+                        },
+                        (err) => {
+                            callback(err, 'SUCCESS');
+                        }
+                    );
+                }
+            ], (err, result) => {
+                callback(err, result);
+            });
+        }
+
+    ], (err, result) => {
+        console.log('DELETE USER... ERROR ENCOUNTERED:', err);
+        console.log('DELETE FOLLOWS...', result[0]);
+        console.log('DELETE VOTES...', result[1]);
+        console.log('DElETE POSTS...', result[2]);
+        console.log('DELETE COMMENTS...', result[3]);
+    });
 });
 
 module.exports = mongoose.model('User', userSchema, 'user');
